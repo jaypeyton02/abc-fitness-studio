288b510e10c6feb6530bb764ea3c982f07760720
jaypeyton02 <jaypeyton02@gmail.com>
2026-01-27 19:28:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -38,3 +38,32 @@ if (contactForm) {
     alert("Thank you for your message.");
   });
 }
+const addToCartButtons = document.querySelectorAll(".add-to-cart");
+
+addToCartButtons.forEach(button => {
+  button.addEventListener("click", function () {
+    alert("Item added to the cart.");
+  });
+});
+const clearCartBtn = document.getElementById("clearCartBtn");
+
+if (clearCartBtn) {
+  clearCartBtn.addEventListener("click", function () {
+    alert("Cart cleared.");
+  });
+}
+const processOrderBtn = document.getElementById("processOrderBtn");
+
+if (processOrderBtn) {
+  processOrderBtn.addEventListener("click", function () {
+    alert("Thank you for your order.");
+  });
+}
+const contactForm = document.getElementById("contactForm");
+
+if (contactForm) {
+  contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Thank you for your message.");
+  });
+}
