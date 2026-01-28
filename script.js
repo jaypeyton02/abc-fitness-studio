266747d61b0011469f5adce43eb5033a04043721
jaypeyton02 <jaypeyton02@gmail.com>
2026-01-27 19:54:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -67,3 +67,45 @@ if (contactForm) {
     alert("Thank you for your message.");
   });
 }
+const addToCartButtons = document.querySelectorAll(".add-to-cart");
+
+addToCartButtons.forEach(button => {
+  button.addEventListener("click", function () {
+    const item = this.getAttribute("data-item");
+
+    let cart = JSON.parse(sessionStorage.getItem("cartItems")) || [];
+    cart.push(item);
+
+    sessionStorage.setItem("cartItems", JSON.stringify(cart));
+    alert("Item added to the cart.");
+  });
+});
+const viewCartBtn = document.getElementById("viewCartBtn");
+
+if (viewCartBtn) {
+  viewCartBtn.addEventListener("click", function () {
+    let cart = JSON.parse(sessionStorage.getItem("cartItems")) || [];
+
+    if (cart.length === 0) {
+      alert("Your cart is empty.");
+    } else {
+      alert("Your Cart:\n" + cart.join("\n"));
+    }
+  });
+}
+const clearCartBtn = document.getElementById("clearCartBtn");
+
+if (clearCartBtn) {
+  clearCartBtn.addEventListener("click", function () {
+    sessionStorage.removeItem("cartItems");
+    alert("Cart cleared.");
+  });
+}
+const processOrderBtn = document.getElementById("processOrderBtn");
+
+if (processOrderBtn) {
+  processOrderBtn.addEventListener("click", function () {
+    sessionStorage.removeItem("cartItems");
+    alert("Thank you for your order.");
+  });
+}
